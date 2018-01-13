b136947abf0193eece12538c2e8d1e4085315194
karimshakir <karimshakir99@gmail.com>
2018-01-12 18:03:42 -0600

    first commit

---
+++ src/com/karimshakir/todolist/Controller.java
@@ -0,0 +1,9 @@
+package com.karimshakir.todolist;
+
+import com.karimshakir.todolist.datamodel.TodoItem;
+
+import java.util.List;
+
+public class Controller {
+    private List<TodoItem> todoItemList;
+}
